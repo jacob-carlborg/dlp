8791c6966ea59a7cae8b3c350e4f2dac152fb28c
Jacob Carlborg <doob@me.com>
2020-12-04 20:28:14 +0100

    fixup! Disable Travis CI and AppVeyor

---
+++ tools/install_dc.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -eu
+set -o pipefail
+
+function d_compiler {
+  case "${DLP_COMPILER}" in
+    'dmd-latest') echo 'dmd' ;;
+    'ldc-latest') echo 'ldc' ;;
+    'dmd-master') echo 'dmd-nightly' ;;
+    'ldc-master') echo 'ldc-latest-ci' ;;
+    *) echo "${DLP_COMPILER}" ;;
+  esac
+}
+
+function install_compiler {
+  if [ "$(uname)" = 'FreeBSD' ]; then
+    local compiler="$(d_compiler)"
+    curl -sS -L https://github.com/dlang/installer/raw/d97cec87e464a703c44128d72c9ba89576df6e5c/script/install.sh | bash -s "${compiler}"
+    source "$(~/dlang/install.sh "${compiler}" -a)"
+  else
+    local DMD="$([ "$DC" = 'ldc2' ] && echo 'ldmd2' || echo 'dmd')"
+
+    if [ ${CI:-false} = true ]; then
+      echo "DMD=$DMD" >> $GITHUB_ENV
+    fi
+  fi
+}
+
+install_compiler
